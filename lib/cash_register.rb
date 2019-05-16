1edf175c1e2d565cc633807d0d6ee598c30dd300
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-16 07:07:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,9 +3,10 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction_amount, :items
 
   def initialize(discount = 0)
+    @items = []
     @total = 0
     @discount = discount
-    @items = []
+    
   end
 
 
